7daf9b6823882ef8f9e0dc636547a380b4c3d8ca
framlin <w.egger@framlin.de>
2012-03-03 17:43:01 +0100

    add content use jquery to navigate

--- _attachments/js/script.js
+++ _attachments/js/script.js
@@ -1,9 +1,35 @@
 /* Author: Wolfgang Egger
 */
-function styling() {
-	//topteaserStyling();
+
+function styleArticles() {
+	$('section').addClass('hidden');
+}
+function styleContent() {
+	$('#content').addClass('hidden');
+}
+
+function activatePage() {
+	activateNavigation();
+}
+
+function showSection(id) {
+	styleArticles();
+	$(id).removeClass('hidden');
+}
+
+function activateNavigation() {
+	$('nav a').click(function onClickA(){
+		//styleArticle();
+		var target = '#sec_' + $(this).attr('href').substring(1);
+		showSection(target);
+	});
+}
+
+function stylePage() {
+	styleArticles();
 }
 
 jQuery(function() {
-	//styling();
+	stylePage();
+	activatePage();
 });
