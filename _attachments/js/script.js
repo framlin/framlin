559819734c6b04d853e613fb9d87b89be327aec7
framlin <w.egger@framlin.de>
2012-03-04 20:56:08 +0100

    show teaser on hover

--- _attachments/js/script.js
+++ _attachments/js/script.js
@@ -1,31 +1,81 @@
 /* Author: Wolfgang Egger
-*/
+ */
+var Framlin = (function(window){
+	var $ = null;
+	
+	function Module() {
+		this.initialized = false;
+		this.currentSection = '';
 
-function hideArticles() {
-	$('section').addClass('hidden');
-}
+	}
+	
+	Module.prototype.init = function init() {
+		$ = window.$;
+	};
 
-function showSection(id) {
-	hideArticles();
-	$(id).removeClass('hidden');
-}
 
-function activateNavigation() {
-	$('nav a').click(function onClickA(){
-		var target = '#sec_' + $(this).attr('href').substring(1);
-		showSection(target);
-	});
-}
+	Module.prototype.hideArticles = function hideArticles() {
+		$('section').addClass('hidden');
+		if (!this.initialized) {
+			$('#sec_home').removeClass('hidden');
+		}
+	};
 
-function activatePage() {
-	activateNavigation();
-}
+	Module.prototype.showSection = function showSection(id) {
+		this.hideArticles();
+		$(id).removeClass('hidden');
+		this.currentSection = id;
+	};
 
-function stylePage() {
-	hideArticles();
-}
+	Module.prototype.showHeader = function showHeader(id) {
+		var teaser = $('#teaser'),
+		clone = $(id + ' header').clone();
+
+		teaser.removeClass('hidden');
+		teaser.wrapInner(clone);		
+	};
+
+	Module.prototype.hideHeader = function hideHeader(id) {
+		$('#teaser header').remove();
+		$('#teaser').addClass('hidden');
+	};
+
+	Module.prototype.activateNavigation = function activateNavigation() {
+		var me = this;
+		$('nav a').click(function onClickA(){
+			var target = '#sec_' + $(this).attr('href').substring(1);
+			me.showSection(target);
+			me.hideHeader(target);
+		});
+
+		$('nav a').hover(function onInA(){
+			var target = '#sec_' + $(this).attr('href').substring(1);
+			if (target !== me.currentSection) {
+				me.showHeader(target);
+			}
+		}, function onOutA(){
+			var target = '#sec_' + $(this).attr('href').substring(1);
+			me.hideHeader(target);
+		});
+	};
+
+	Module.prototype.activatePage = function activatePage() {
+		this.activateNavigation();
+	};
+
+	Module.prototype.stylePage = function stylePage() {
+		this.hideArticles();
+		this.initialized = true;
+	};
+
+	// ###############################
+	return new Module();
+
+})(window);
+
 
 jQuery(function() {
-	stylePage();
-	activatePage();
+	Framlin.init();
+	Framlin.stylePage();
+	Framlin.activatePage();
 });
