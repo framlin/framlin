f81e5d9cdef09c46c18619dff7fc1aa5199fa805
framlin <w.egger@framlin.de>
2012-03-18 07:27:28 +0100

    optimize content fadein/-out, use optimizer and shadows, animate nafigation-box

--- _attachments/js/script.js
+++ _attachments/js/script.js
@@ -7,7 +7,6 @@ var Framlin = (function(window){
 		this.initialized = false;
 		this.currentSection = '#sec_home';
 		this.cameIn = false;
-
 	}
 	
 	Module.prototype.init = function init() {
@@ -15,7 +14,13 @@ var Framlin = (function(window){
 	};
 
 
+	Module.prototype.hideArticle = function hideArticle(id) {
+		console.log('hideArticle', id);
+		$(id).fadeOut(20);
+	};
+
 	Module.prototype.hideArticles = function hideArticles() {
+		console.log('hideArticles');
 		$('section').fadeOut(20);
 		if (!this.initialized) {
 			$('#sec_home').fadeIn(20);
@@ -23,20 +28,28 @@ var Framlin = (function(window){
 	};
 
 	Module.prototype.showSection = function showSection(id, time) {
+		console.log('showSection', id);
 		this.hideArticles();
 		$(id).fadeIn(time||20);
 		this.currentSection = id;
 	};
 
 	Module.prototype.showHeader = function showHeader(id) {
+		this.hideArticle(this.currentSection);
+		console.log('showHeader', id);
 		var teaser = $('#teaser'),
 		clone = $(id + ' header').clone();
-		this.hideArticles();
-		teaser.fadeIn(20);
-		teaser.wrapInner(clone);		
+		var more = $('<p>klick to read more ...</p>');
+		more.addClass('more');
+		clone.append(more);
+		teaser.wrapInner(clone);
+		window.setTimeout(function fadeIn() {
+			teaser.fadeIn(400);
+		}, 100);
 	};
 
 	Module.prototype.hideHeader = function hideHeader(id) {
+		console.log('hideHeader', id);
 		$('#teaser').fadeOut(20);
         $('#teaser header').remove();
 	};
@@ -65,17 +78,50 @@ var Framlin = (function(window){
                 var target = '#sec_' + $(this).attr('href').substring(1);
                 if ((!me.cameIn) && (target !== me.currentSection)) {
                     me.hideHeader(target);
-                    me.showSection(me.currentSection);
+                    window.setTimeout(function showSection() {
+                        me.showSection(me.currentSection, 400);
+                    }, 100);
                 }
                 me.cameIn = false;
             });
+
+		$('#topteaser').hover(
+				function onInA(){
+					$(this).removeClass('box-high');
+					$(this).addClass('box-flat');
+					$(this).css('margin-left', '-=1');
+					$(this).css('margin-top', '-=1');
+					$(this).css('margin-bottom', '+=1');
+				}, function onOutA(){
+					$(this).removeClass('box-flat');
+					$(this).addClass('box-high');
+					$(this).css('margin-left', '+=1');
+					$(this).css('margin-top', '+=1');
+					$(this).css('margin-bottom', '-=1');
+				});
 	};
 
+	
+
+	
+	
+	Module.prototype.activateHref = function activateHref() {
+		$('nav a').each(function(index, anchor){
+			$(anchor).attr('href', '#'+$(anchor).attr('href'));
+		});
+	};
+	
 	Module.prototype.activatePage = function activatePage() {
+		this.activateHref();
 		this.activateNavigation();
 	};
 
+	Module.prototype.showContent = function showContent() {
+		$('#content').removeClass('hidden');
+	};
+	
 	Module.prototype.stylePage = function stylePage() {
+		this.showContent();
 		this.hideArticles();
 		this.initialized = true;
 	};
@@ -83,11 +129,15 @@ var Framlin = (function(window){
 	// ###############################
 	return new Module();
 
-})(window);
-
-
-jQuery(function() {
-	Framlin.init();
-	Framlin.stylePage();
-	Framlin.activatePage();
 });
+
+if (jQuery) {
+	jQuery(function() {
+		var framlin = Framlin(window);
+		framlin.init();
+		framlin.stylePage();
+		framlin.activatePage();
+	});	
+}
+
+exports = Framlin;
