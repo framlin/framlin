e1561a67d45aad2be805a6149d10515d08909919
Wolfgang Egger <w.egger@framlin.com>
2018-04-14 12:17:30 +0200

    move ci-styles to ci-service

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 WDIR=$1
-forever start -w --watchDirectory $WDIR -l framlin.log -a $WDIR/server/run.js
+forever start -w --watchDirectory $WDIR -l framlin.log -a $WDIR/node_modules/http-server/bin/http-server -p 8080
