ab0196b18617279174eeb09997d647b497cef218
Wolfgang Egger <w.egger@framlin.com>
2018-04-14 16:41:47 +0200

    consolidate site move everything to services

--- server/builder.js
+++
@@ -1,5 +0,0 @@
-var frBuilder = require('fr-builder'),
-    PARTIALS_PATH = __dirname + '/../site/partials/',
-    localPartialUrls = frBuilder.listAllPartials(PARTIALS_PATH);
-
-module.exports = new frBuilder(localPartialUrls);
